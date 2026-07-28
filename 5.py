2ecaddc00191cd160bb8f8a1b9c58f6429dfc2cf
guneshsingh30 <guneshsingh30@gmail.com>
2026-07-29 01:11:25 +0530

    enhanced

--- 5.py
+++ 5.py
@@ -1,4 +1,4 @@
-
+un=0
 v=3
 while v>0:
     if un == "12345":
@@ -7,8 +7,11 @@ while v>0:
 
     
     else:
+        print(f"{v} chances left")
         un=input("Enter user naame")
         v-=1
         continue
+    
+        
     
 
